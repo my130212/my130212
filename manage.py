080c40e8d80686191811d184eed74e225a1b133c
mayong <1552495348@qq.com>
2021-06-16 20:53:42 +0800

    将config的配置，抽取成config模块

--- manage.py
+++ manage.py
@@ -8,30 +8,7 @@ from redis import StrictRedis
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 
-class Config(object):
-	"""项目的配置"""
-	Debug = True
-
-	SECRET_KEY = 'zRDv7adXxniygRZvZnAZLLVV2fmY8rhNmdkFZ7xBoVk4ivMV4yU5inhKtxCuYdQ+sJYDm324FLerIkHZknS2gg=='
-
-	#为数据库添加配置
-	SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/3306/mydb'
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-	#添加redis数据的配置
-	REDIS_HOST = '127.0.0.1'
-	REDIS_PORT = 6379
-
-	#session的保存位置
-	SESSION_TYPE = 'redis'
-
-	SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT,db=1)
-	#开启session签名
-	SESSION_USE_SIGNER = True
-	#设置session需过期
-	SESSION_PERMANENT = False
-	#设置session过期时间
-	PERMANENT_SESSION_LIFETIME = 86400 * 2
+from config import Config
 
 app = Flask(__name__)
 #加载配置
