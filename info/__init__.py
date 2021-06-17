d2b79a93d3fdd4f736bc0f2c625bc55e19287918
mayong <1552495348@qq.com>
2021-06-17 22:30:47 +0800

    将app中业务配置包装成函数create_app,在主函数中通过传参，选择不同的环境

--- info/__init__.py
+++ info/__init__.py
@@ -6,14 +6,20 @@ from flask_wtf import CSRFProtect
 
 from config import config
 
-app = Flask(__name__)
-#加载配置
-app.config.from_object(config['devolopment'])
 #初始话数据库
-db = SQLAlchemy(app)
-#初始话redis数据库存储
-redis_store = FlaskRedis(app)
-#开启当前项目 csrf 保护，只做服务器验证功能
-CSRFProtect(app)
-#设置Session的指定保存位置
-Session(app)
+db = SQLAlchemy()
+
+def creat_app(config_name):
+	app = Flask(__name__)
+	#加载配置
+	app.config.from_object(config[config_name])
+	#数据库添加配置
+	db.init_app(app)
+	#初始话redis数据库存储
+	redis_store = FlaskRedis(app)
+	#开启当前项目 csrf 保护，只做服务器验证功能
+	CSRFProtect(app)
+	#设置Session的指定保存位置
+	Session(app)
+
+	return app
--- manage.py
+++ manage.py
@@ -2,8 +2,10 @@ from flask import session
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 
-from info import app,db
+from info import creat_app,db
 
+#创建app，配置不同环境的配置
+app = creat_app('devolopment')
 #使用项目管理器管理app
 manage = Manager(app)
 #设置迁移Migrate对象,将app与db相关联
@@ -13,6 +15,7 @@ manage.add_command('db',MigrateCommand)
 
 @app.route("/")
 def index():
+
 	session['nan'] = '26'
 	return "<h1 style='color:red'>Hello World</h1>"
 
